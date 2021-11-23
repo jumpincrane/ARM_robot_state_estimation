fb8b2e8dccb5b483ced1bea2b1a30bfd94e8c2a5
Michal Heit <michalnt98@gmail.com>
2021-11-23 12:05:15 +0100

    added input_generator and changed robot_node

---
+++ scripts/input_generator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+import numpy as np
+
+
+
+def signal_gen():
+    pub = rospy.Publisher("input_signal", Twist, queue_size=10)
+    rospy.init_node('input_generator', anonymous=True)
+    rate = rospy.Rate(30)
+    signal = Twist()
+    i = 0
+    while not rospy.is_shutdown():
+        signal.linear.x = np.sin(2 * np.pi * i / 4000) * 20
+        signal.linear.y = np.cos(2 * np.pi * i / 3000) * 20
+        signal.linear.z = 0
+        signal.angular.x = 0
+        signal.angular.y = 0
+        signal.angular.z = np.sin(2 * np.pi * i / 1000) * 30
+        pub.publish(signal)
+        rospy.loginfo(signal)
+        i = i + 1
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        signal_gen()
+    except rospy.ROSInterruptException:
+        pass
--- scripts/robot_node.py
+++ scripts/robot_node.py
@@ -1,69 +1,67 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist, Pose, Point, Quaternion, Vector3
-from tf.transsformations import euler_from_quaternioin
+from tf.transformations import euler_from_quaternion
 from nav_msgs.msg import Odometry
 import numpy as np
 import time
 
+input_data = [0, 0, 0]
+
 
 def callback_input(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    global input_data
     u1 = data.linear.x
     u2 = data.linear.y
+    theta = data.angular.z
 
-    # Actualization of the Odometry message to bo published
-    x = data.odometry.pose.position.x
-    y = data.odometry.pose.position.y
-    beta = data.odometry.pose.position.z
-    quat = data.odometry.pose.pose.rotation
-    theta = euler_from_quaternioin([quat.x, quat.y, quat.z, quat.w])[2]
-
-    # theta = data.angular.z
+    global input_data
     input_data = [u1, u2, theta]
-    
 
 
-def calculations(u1, u2, theta, t):
+def calculations(u1, u2, theta, delta_t, t):
     our_odom = Odometry()
-    u1, u2, theta = input_data
-    beta = u1 * t
+    beta = u1 * delta_t
     l = 1
-    v1 = u2/l * np.tan(beta)
+    v1 = u2 / l * np.tan(beta)
     v2 = u2
     matrixA = np.array([[1, 0],
                         [0, np.cos(theta)],
                         [0, np.sin(theta)]])
     v = np.array([v1, v2])
-    
-    q_d = matrixA @ v
-    
-    # trzeba przypisac wynik do zmiennej rosowej our_odom
-    q_d[0]
-    q_d[1]
-    q_d[2]
 
+    q_d = matrixA @ v
+
+    # trzeba przypisac wynik do zmiennej rosowej our_odom
+    our_odom.header.frame_id = '/map'
+    our_odom.header.stamp = t
+    our_odom.pose.pose = Pose(Point(0, 0, 0), Quaternion(0, 0, 0, 1))
+    our_odom.twist.twist = Twist(Vector3(q_d[0], q_d[1], 0), Vector3(0, 0, q_d[2]))
     return our_odom
 
 
 def main():
     odometry_pub = rospy.Publisher('Odometry_pub', Odometry, queue_size=10)
-    rospy.Subscriber("sub_input_data", Twist, callback_input)
+    rospy.Subscriber("/input_signal", Twist, callback_input)
     rospy.init_node('robot_node', anonymous=True)
     rate = rospy.Rate(1)  # 100hz
-
+    i = 0
+    our_odom = Odometry()
     while not rospy.is_shutdown():
+
         if i == 0:
             t0 = time.time()
-        
+            t2 = 0
+            our_odom.header.frame_id = '/map'
+            our_odom.header.stamp = t0
+            our_odom.pose.pose = Pose(Point(0, 0, 0), Quaternion(0, 0, 0, 1))
+            our_odom.twist.twist = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+
         t = time.time() - t0
         delta_t = t - t2
-        
-        our_odom = calculations(u1, u2, delta_t)
-        our_odom.header.timestamp = t
-        
+        our_odom = calculations(input_data[0], input_data[1], input_data[2], delta_t, t)
         odometry_pub.publish(our_odom)
+        print(our_odom)
+
         i += 1
         rate.sleep()
         t2 = time.time() - t0
